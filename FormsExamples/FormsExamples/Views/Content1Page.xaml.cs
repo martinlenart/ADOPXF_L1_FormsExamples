8f311bd67e19331d3a626df4d0111451bea630f8
Martin Lenart <martin_lenart@icloud.com>
2022-01-10 20:01:23 +0100

    Add project files.

---
+++ FormsExamples/FormsExamples/Views/Content1Page.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace FormsExamples.Views
+{
+	public partial class Content1Page : ContentPage
+	{
+		public Content1Page ()
+		{
+			InitializeComponent ();
+		}
+
+		protected void ButtonClicked(object sender, EventArgs e) {
+
+			((Button)sender).Text = "It is so!";
+		}
+
+		async protected void ImageTapped(object sender, EventArgs e) {
+
+			Image image = ((Image)sender);
+
+			image.Opacity = .5;
+			await Task.Delay(200);
+			image.Opacity = 1;
+		}
+	}
+}
